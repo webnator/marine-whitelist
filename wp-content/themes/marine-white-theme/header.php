26239220003da6002021033daebd8c6dae727032
Williams Aguilera <webnator@gmail.com>
2015-10-04 20:11:39 +0200

    Added responsive header slideve header slider

--- wp-content/themes/marine-white-theme/header.php
+++ wp-content/themes/marine-white-theme/header.php
@@ -161,6 +161,9 @@
 <div class="container-fluid" style="padding-left:0px;">
 	<div class='container'>
 		<div class="row">
+			<div class="col-md-12">
+				<?php putRevSlider("home-slider"); ?>
+			</div>
 			<div class="col-md-10 col-md-offset-1 col-sm-12">
 				<div class="row">
 
