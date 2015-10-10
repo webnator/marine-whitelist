82257d6ca611c4c5fac0be4be60fde818a6a7c29
Williams Aguilera <webnator@gmail.com>
2015-10-10 18:46:39 +0200

    ADD: Footer; MOD: Main screen

--- wp-content/themes/marine-white-theme/footer.php
+++ wp-content/themes/marine-white-theme/footer.php
@@ -1,4 +1,65 @@
+      <div class="row widget-container">
+        <div class="col-md-10 col-md-offset-1">
+          <div class="row">
+            <div class="col-md-4 foot-widget">
+              <h1>Marine Florida</h1>
+              198 West 21th Street, Suite 721 <br>
+              New York NY 10010 <br>
+              Email: marine@whitelist.com <br>
+              Phone: +88 (0) 101 0000 000 <br>
+              Fax: +88 (0) 202 0000 001  <br>
+            </div>
+            <div class="col-md-4 foot-widget">
+              <ul>
+                <li>
+                  <a href="">FAQ</a>
+                </li>
+                <li>
+                  <a href="">Privacy Policy</a>
+                </li>
+                <li>
+                  <a href="">Contact Us</a>
+                </li>
+                <li>
+                  <a href="">Abour us</a>
+                </li>
+              </ul>
+              
+            </div>
+            <div class="col-md-4 foot-widget">
+              <ul>
+                <li>
+                  <a href="">
+                    <i class="fa fa-facebook-official"></i>
+                    Facebook
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    <i class="fa fa-twitter-square"></i>
+                    Twitter
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    <i class="fa fa-google-plus-square"></i>
+                    Google+
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row footer">
+        <div class="col-xs-12">
+          © 2015 Marine White List, LLC. All rights reserved
+        </div>
+      </div>
 		</div>
 	</div>
+
+
 
 <?php wp_footer(); ?>
--- wp-content/themes/marine-white-theme/home-screen.php
+++ wp-content/themes/marine-white-theme/home-screen.php
@@ -11,18 +11,159 @@
   </div>
 </div>
 <div class="row split-row">
-  <div class="col-md-3 col-md-offset-2">
-    <span class="glyphicon glyphicon-check" aria-hidden="true"></span> Always free
+  <div class="col-md-4">
+    <i class="fa fa-check"></i> Always free
   </div>
 
-  <div class="col-md-2">
-    <span class="glyphicon glyphicon-check" aria-hidden="true"></span> We'll research for you
+  <div class="col-md-4">
+    <i class="fa fa-check"></i> We'll research for you
   </div>
 
-  <div class="col-md-3">
-    <span class="glyphicon glyphicon-check" aria-hidden="true"></span> Book online
+  <div class="col-md-4">
+    <i class="fa fa-check"></i> Book online
   </div>
 </div>
+
+<div class="row" style="margin-bottom: 30px">
+  <div class="col-md-10 col-md-offset-1">
+
+    <div class="row">
+      <div class="col-md-12">
+        <span class="big-title dark-contrast">Service Categories</span>
+        <hr>
+      </div>
+    </div>
+
+    <div class="row">
+      
+      <div class="col-md-3 col-xs-6">
+        <div class="row">
+          <div class="col-xs-4">
+            <img 
+              class="img-responsive"
+              src="http://marinewhitelist.com/wp-content/uploads/2015/09/electrical.png"/>
+          </div>
+          <div class="col-xs-8 cat-title">
+            Charters
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-xs-6">
+        <div class="row">
+          <div class="col-xs-4">
+            <img 
+              class="img-responsive"
+              src="http://marinewhitelist.com/wp-content/uploads/2015/09/electrical.png"/>
+          </div>
+          <div class="col-xs-8 cat-title">
+            Charters
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-xs-6">
+        <div class="row">
+          <div class="col-xs-4">
+            <img 
+              class="img-responsive"
+              src="http://marinewhitelist.com/wp-content/uploads/2015/09/electrical.png"/>
+          </div>
+          <div class="col-xs-8 cat-title">
+            Charters
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-xs-6">
+        <div class="row">
+          <div class="col-xs-4">
+            <img 
+              class="img-responsive"
+              src="http://marinewhitelist.com/wp-content/uploads/2015/09/electrical.png"/>
+          </div>
+          <div class="col-xs-8 cat-title">
+            Charters
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-xs-6">
+        <div class="row">
+          <div class="col-xs-4">
+            <img 
+              class="img-responsive"
+              src="http://marinewhitelist.com/wp-content/uploads/2015/09/electrical.png"/>
+          </div>
+          <div class="col-xs-8 cat-title">
+            Charters
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-xs-6">
+        <div class="row">
+          <div class="col-xs-4">
+            <img 
+              class="img-responsive"
+              src="http://marinewhitelist.com/wp-content/uploads/2015/09/electrical.png"/>
+          </div>
+          <div class="col-xs-8 cat-title">
+            Charters
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-xs-6">
+        <div class="row">
+          <div class="col-xs-4">
+            <img 
+              class="img-responsive"
+              src="http://marinewhitelist.com/wp-content/uploads/2015/09/electrical.png"/>
+          </div>
+          <div class="col-xs-8 cat-title">
+            Charters
+          </div>
+        </div>
+      </div>
+
+      
+
+
+    </div>
+
+  </div>
+</div>
+
+
+
+<div class="row tile">
+  <div class="col-md-10 col-md-offset-1">
+    <span class="big-title">Our latest review</span>
+    <hr>
+    <div class="row">
+      <div class="col-md-10 col-md-offset-1 review-box">
+        <div class="row">
+          <div class="col-md-8">
+            Jacky is wonderful - her crew cleaned my 35' sunseeker last week, and it is probably the cleanest the boat has been since it was built. 
+          </div>
+          <div class="col-md-4">
+            <div class="row">
+              <div class="col-md-12">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-o"></i>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12">    
+                <i>Maggie Fox</i> at <strong>Home</strong>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 
 <?php get_footer(); ?>
